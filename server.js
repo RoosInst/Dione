fcd532b0a1afec2c52af86ce0911f235fe8e22d5
Ryan <ryan.benech@gmail.com>
2019-03-19 16:28:15 -0700

    note: dev port set by vscode launch

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const compression = require('compression');
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;  //set by launch script
 const app = express();
 
 app.use(compression());
