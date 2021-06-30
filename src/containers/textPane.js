a936abb501f9e4c1dec6e54ff9ad22d00eb69d61
Aimee Kerr <akerr@roos.com>
2021-06-30 14:21:24 -0700

    Initial changes to new context menu package

--- src/containers/textPane.js
+++ src/containers/textPane.js
@@ -1,15 +1,14 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { convertObjToArrayForPublish } from '../scripts/functions';
-import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
-import { addSelection } from '../actions';
+import { addSelection, updatePaneSize } from '../actions';
 import eol from 'eol'; //some line endings inconsistent (CR with CRLF)
 import PropTypes from 'prop-types';
-
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import { mqttClient, cellID } from '../containers/mqtt';
 
 class Pane extends Component {
-
   static propTypes = {
     clientID: PropTypes.string.isRequired,
     model: PropTypes.string.isRequired,
@@ -36,19 +35,34 @@ class Pane extends Component {
 
 	render() {
     this.handleClick = this.handleClick.bind(this);
-    const { obj } = this.props;
+    const { obj, paneSize, updatePaneSize } = this.props;
 
+    const handleClick1 = (event) => {
+      event.preventDefault();
+      let mouseX = event.clientX;
+      let mouseY = event.clientY;
+      updatePaneSize('MousePosition',mouseY,mouseX);
+      this.forceUpdate();
+    };
+
+    const handleClose = () => {
+      this.mouseY = null;
+      this.mouseX = null;
+      updatePaneSize('MousePosition',this.mouseY,this.mouseX)
+      this.forceUpdate();
+    };
+
+    //updatePaneSize('MousePosition',null,null);
     for (let key in obj) { //Check for "*Menu" obj inside current obj, ex. wbMenu, textMenu. Will be used for right click context menu
       if (key.includes("Menu")) {
         var menu = key; //var, not let
       }
     }
      if (obj.identifier && menu && obj[menu].value) { //if right-clicking capabilities
-       return (
-         <div className="contextMenu shell">
-           <ContextMenuTrigger id={obj.identifier}>
-
-                {obj.contents && obj.contents[0] ?
+      console.info(paneSize.MousePosition.height); 
+      return (
+         <div id={obj.identifier} onContextMenu={handleClick1} style={{ height: '50px', width: '50px', cursor: 'context-menu' }}>
+              {  obj.contents && obj.contents[0] ?
                    Array.isArray(obj.contents) && obj.contents[0].highlight ? //if highlight exists, then assuming array will only be length 1 (contents[0]). contents[0] sometimes undefined, so check
                     <pre>
                       {obj.contents[0].text.substring(0, obj.contents[0].highlight[0] - 1)}
@@ -59,23 +73,29 @@ class Pane extends Component {
                     </pre>
                   : //no highlight
                     <pre>{eol.lf(obj.contents[0].text)}</pre>
-                : <span style={{whiteSpace: 'pre'}} />
-                //ContextMenuTrigger needs child, so render empty span
+                  : <span style={{whiteSpace: 'pre'}} />
+                 //ContextMenuTrigger needs child, so render empty span
               }
-           </ContextMenuTrigger>
-           <ContextMenu id={obj.identifier}>
+           <Menu keepMounted
+            open={paneSize.MousePosition.height !== null}
+            onClose={handleClose}
+            anchorReference="anchorPosition"
+            anchorPosition={ paneSize.MousePosition.height !== null && paneSize.MousePosition.width !== null
+              ? { top: paneSize.MousePosition.height, left: paneSize.MousePosition.width }
+              : undefined }
+            >
              {
                obj[menu].value.map((menuItem, key) => {
                  if (menuItem) {
                  return(
-                   <MenuItem key={key} onClick={() => this.handleClick(menuItem, obj[menu])}>
+                   <MenuItem key={key} onClick={() => handleClose()}>
                        {menuItem.text}
                    </MenuItem>
                  );
                } else return
              })
             }
-          </ContextMenu>
+          </Menu>
          </div>
        );
      }
@@ -91,8 +111,9 @@ function mapStateToProps(state) {
   return {
 		clientID: state.clientID,
 		whiteboard: state.whiteboard,
-    selectedItems: state.selectedItems
+    selectedItems: state.selectedItems,
+    paneSize: state.paneSize
   };
 }
 
-export default connect(mapStateToProps, { addSelection } )(Pane);
+export default connect(mapStateToProps, { addSelection, updatePaneSize } )(Pane);
--- src/reducers/reducer_paneSizes.js
+++ src/reducers/reducer_paneSizes.js
@@ -1,9 +1,10 @@
 import { UPDATE_PANE_SIZES } from '../actions';
 
-export default function(state = {}, action) {
+export default function(state = {MousePosition: {height: null, width: null}}, action) {
   switch(action.type) {
     case UPDATE_PANE_SIZES:
         state[action.model] = {height: action.height, width: action.width};
+        console.info(state);
         return state;
   }
   return state;
