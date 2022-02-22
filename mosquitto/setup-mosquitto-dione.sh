b19d67b70d826b2b51ea4f0e728a11c0a119d04f
Ryan Benech <rbenech@roos.com>
2022-02-22 11:55:45 -0500

    restart brew service after config change

--- mosquitto/setup-mosquitto-dione.sh
+++ mosquitto/setup-mosquitto-dione.sh
@@ -4,3 +4,4 @@ cp ./mosquitto.min.mac.conf /usr/local/etc/mosquitto/mosquitto.conf
 cp ./dione-mosquitto.pem /usr/local/etc/mosquitto/
 cp ./dione-mosquitto.key /usr/local/etc/mosquitto/
 cp ./pwdfile.rtalk /usr/local/etc/mosquitto/
+brew services restart mosquitto
