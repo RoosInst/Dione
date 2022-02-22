66a5763b50095953d2070a40436b45a5fb1ccb18
Ryan Benech <rbenech@roos.com>
2022-02-22 11:39:38 -0500

    mosquitto setup scripts and updated .min.mac.conf

---
+++ mosquitto/setup-mosquitto-dione.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# run from Dione working directory
+cp ./mosquitto.min.mac.conf /usr/local/etc/mosquitto/mosquitto.conf
+cp ./dione-mosquitto.pem /usr/local/etc/mosquitto/
+cp ./dione-mosquitto.key /usr/local/etc/mosquitto/
+cp ./pwdfile.rtalk /usr/local/etc/mosquitto/
